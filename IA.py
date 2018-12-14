ae1724f6bcc94307c3f0e96dbfb95458ca2a01d9
Clement Gobe <gobec@ens-epua.univ-savoie.fr>
2018-12-14 11:52:02 +0100

    Joueur contre Programme(IA random) avec interface textuel

--- IA.py
+++ IA.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 import chess
+import random
 
+def IAMove(board):
+    moves = board.legal_moves
+    numero = random.randint(0,moves.count())
+    i=0
+    for move in moves:
+        if i==numero:
+            return move
+        i+=1
--- JvsP.py
+++ JvsP.py
@@ -1,42 +1,86 @@
 # -*- coding: utf-8 -*-
 import chess
-import IA from IA
-
-#Choix
-typeGame = input("Choose your game type (0:PvsP | 1:JvsP): ")
-if typeGame == "1":
-    color = input("Choose your color (0: white | 1: black): ")
+import IA
     
-#Initialisation
+#Initialization
+color = 0
 board = chess.Board()
-print(board)
 
-#Gestion des tours
-def ProgramTurn():
-    move=IAMove()
-    board.push(chess.Move.from_uci(move))
-
-def PlayerTurn():
+##############################################################################
+def ProgramTurn(board):
+    '''
+        Call IA to play the program turn and play the move.
+        
+        PARAM board{chess.Board} --> the board of the game
+        RETURN {None}
+    '''
+    
+    move=IA.IAMove(board)
+    board.push(move)
+    
+##############################################################################
+def PlayerTurn(board):
+    '''
+        Ask Player to enter a valid move and play the move or end the game with draw.
+        
+        PARAM board{chess.Board} --> the board of the game
+        RETURN {None}
+    '''
+    
     text="Enter your move"
     if board.can_claim_draw():
         text+="(or claim draw)"
     move=input(text+":")
-    if move in board.legal_moves:
+    if chess.Move.from_uci(move) in board.legal_moves: #move must be  in fromsquare+endsquare format
         board.push(chess.Move.from_uci(move))
     elif move=="draw" and board.can_claim_draw():
         board.is_game_over(claim_draw=True)
     else:
-        PlayerTurn()
-
-Order=[None,None]
-Order[color]=PlayerTurn()
-Order[color-1]=ProgramTurn()
-
-#Partie en cours
-while not(board.is_game_over()):
-    if not(board.is_game_over()):
-        Order[0]
-        print(board)
-    if not(board.is_game_over()):
-        Order[1]
-        print(board)
+        PlayerTurn(board)
+##############################################################################        
+def Turn(n,board):
+    '''
+        Choose the player of the current color turn.
+        
+        PARAM n{int} --> Current color
+        PARAM board{chess.Board} --> the board of the game
+        RETURN PlayerTurn(board) or ProgramTurn(board)
+    '''
+    
+    if n==color:
+        return PlayerTurn(board)
+    else:
+        return ProgramTurn(board)   
+##############################################################################
+def PlayerColor(color):
+    '''
+        Ask the player to choose his/her color.
+        
+        PARAM color{int} --> Player's color
+        RETURN {None}
+    '''
+    
+    color = int(input("Choose your color (0: white | 1: black): "))
+    if color not in [0,1]:
+        PlayerColor()
+##############################################################################     
+def Game(board):
+    '''
+        Play the game until the game is over.
+        
+        PARAM board{chess.Board} --> the board of the game
+        RETURN {None}
+    '''
+    
+    while not(board.is_game_over()):
+        if not(board.is_game_over()):
+            Turn(0,board)
+            print("\n",board)
+        if not(board.is_game_over()):
+            Turn(1,board)
+            print("\n",board)
+##############################################################################      
+#Current game
+PlayerColor(color)
+print(board)
+Game(board)
