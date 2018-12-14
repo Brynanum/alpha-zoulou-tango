f9d045bb6e2cb6e67f12675e97747ce1d37f393f
Brynanum <bryan.fauquembergue@etu.univ-smb.fr>
2018-12-14 09:45:50 +0100

    display piece

--- image.py
+++ image.py
@@ -1,35 +1,15 @@
 # -*- coding: utf-8 -*-
 import chess
 import chess.svg
-from IPython.display import SVG
+from IPython.display import SVG, display
 
 class Image:
-	symbols={
-		'K':SVG(chess.svg.piece(chess.Piece.from_symbol('K'))),
-		'Q':SVG(chess.svg.piece(chess.Piece.from_symbol('Q'))),
-		'B':SVG(chess.svg.piece(chess.Piece.from_symbol('B'))),
-		'N':SVG(chess.svg.piece(chess.Piece.from_symbol('N'))),
-		'R':SVG(chess.svg.piece(chess.Piece.from_symbol('R'))),
-		'P':SVG(chess.svg.piece(chess.Piece.from_symbol('P'))),
-		'k':SVG(chess.svg.piece(chess.Piece.from_symbol('k'))),
-		'q':SVG(chess.svg.piece(chess.Piece.from_symbol('q'))),
-		'b':SVG(chess.svg.piece(chess.Piece.from_symbol('b'))),
-		'n':SVG(chess.svg.piece(chess.Piece.from_symbol('n'))),
-		'r':SVG(chess.svg.piece(chess.Piece.from_symbol('r'))),
-		'p':SVG(chess.svg.piece(chess.Piece.from_symbol('p')))
-	}
-    
-	def display(self):
-		self.displayBoard()
-		self.displaySymbols()
-
-	def displayBoard(self,boardFEN,symbolActive):
+	def display(boardFEN,pieceActivePosition):
 		board = chess.Board(boardFEN)
-		squares = board.attacks(symbolActive)
-		SVG(chess.svg.board(board=board, squares=squares))
+		pieceActivePosition = board.attacks(pieceActivePosition)
+		display_svg(SVG(chess.svg.board(board=board, squares=pieceActivePosition)))
 
-	def displaySymbols(self):
-		[...]
+	def piece(piece):
+		return chess.svg.piece(chess.Piece.from_symbol(piece))
 
-Image.symbols['K']
-Image.displayBoard("8/8/8/8/4N3/8/8/8 w - - 0 1",chess.E4)
+display(SVG(Image.piece('R')))
